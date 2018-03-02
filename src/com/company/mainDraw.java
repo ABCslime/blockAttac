cb5c1a46fb589c0e100017de222797f1484f5e6c
ABCslime <anej.batagelj@gmail.com>
2018-03-03 00:19:05 +0100

    add

--- src/com/company/mainDraw.java
+++ src/com/company/mainDraw.java
@@ -1,24 +1,75 @@
 package com.company;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.JComponent;
+import java.awt.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.*;
 
 public class mainDraw extends JComponent {
 
-    public int x = 50;
-    public int y = 50;
+    Random rand = new Random();
+    //public int x = 500 + rand.nextInt((4000) + 1);
+    int x = 4950;
+    public int y = 500 + rand.nextInt((4000) + 1);
+    public java.util.List<Integer> List = new ArrayList();
+    public int sx;
+    public int sy;
+
+
+
 
     public void paintComponent(Graphics g) {
+
         super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+
+        generator();
+        System.out.println(List);
+        Image img2 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\ANEJ\\Pictures\\igra\\trava.png");
+        for (int i=0;i<=List.size()/2;i =i+2){
+            g2.drawImage(img2, 50*(List.get(i+1)-1)-x%50-25, 50*(List.get(i)-1)-y%50-25, 50, 50, this);
+
+        }
         g.drawLine(0,500,600,500);
-        g.drawRect(x, y, 50, 50);
-        g.fillRect(x, y, 50, 50);
+        Image img1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\ANEJ\\Pictures\\igra\\oseba.png");
+        g2.drawImage(img1, 300,250 , 50, 50,this);
+        g2.finalize();
         g.setColor(Color.BLACK);
     }
 
+    private void generator() {
+
+        sx = x/50-(x%50)/50;
+        sy = y/50-(y%50)/50;
+        System.out.println(sx);
+        System.out.println(sy);
+        for(int i=1; i<=12; i++) {
+
+            for (int m = 1; m <= 14; m++) {
+                int si = sy -7 + i;
+                int sm = sx -8 + m;
+                if (si > -1 && si < 100 && sm > -1 && sm < 100) {
+                    List.add(i);
+                    List.add(m);
+
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+
+
     public void moveRight() {
-        if (x == 540){
+        if (x == 5000){
 
         }
         else{
@@ -40,7 +91,7 @@ public class mainDraw extends JComponent {
     }
 
     public void moveDown() {
-        if (y == 450){
+        if (y == 5000){
 
         }
         else{
--- src/com/company/mainFrame.java
+++ src/com/company/mainFrame.java
@@ -2,42 +2,41 @@ package com.company;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.File;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
-public class mainFrame extends JFrame implements KeyListener{
+public class mainFrame extends JFrame implements KeyListener {
     private mainDraw draw;
 
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()== KeyEvent.VK_RIGHT)
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
             draw.moveRight();
-        else if(e.getKeyCode()== KeyEvent.VK_LEFT)
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT)
             draw.moveLeft();
-        else if(e.getKeyCode()== KeyEvent.VK_DOWN)
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN)
             draw.moveDown();
-        else if(e.getKeyCode()== KeyEvent.VK_UP)
+        else if (e.getKeyCode() == KeyEvent.VK_UP)
             draw.moveUp();
     }
 
     public void keyReleased(KeyEvent e) {
-        if(e.getKeyCode()== KeyEvent.VK_RIGHT)
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
             draw.moveRight();
-        else if(e.getKeyCode()== KeyEvent.VK_LEFT)
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT)
             draw.moveLeft();
-        else if(e.getKeyCode()== KeyEvent.VK_DOWN)
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN)
             draw.moveDown();
-        else if(e.getKeyCode()== KeyEvent.VK_UP)
+        else if (e.getKeyCode() == KeyEvent.VK_UP)
             draw.moveUp();
 
     }
+
     public void keyTyped(KeyEvent e) {
         System.out.println("keyTyped");
     }
 
-    public mainFrame(){
-        this.draw=new mainDraw();
+    public mainFrame() {
+        this.draw = new mainDraw();
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -60,14 +59,20 @@ public class mainFrame extends JFrame implements KeyListener{
                 frame.setVisible(true);
 
                 ImageIcon icon = new ImageIcon("C:\\Users\\ANEJ\\Pictures\\igra\\meč.png");
-                Image img = icon.getImage();
-                Image newing = img.getScaledInstance( 30, 30,  java.awt.Image.SCALE_SMOOTH ) ;
-                icon = new ImageIcon( newing );
-                JButton b=new JButton(icon);
-                b.setBounds(50,510,30,30);
+
+                JButton b = new JButton(resaiz(icon));
+                b.setBounds(50, 510, 30, 30);
                 frame.add(b);
                 frame.setLayout(null);
             }
         });
+    }
+
+    private static ImageIcon resaiz(ImageIcon icon) {
+        Image img = icon.getImage();
+        Image newing = img.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
+        icon = new ImageIcon(newing);
+        return icon;
+
     }
 }
